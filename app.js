b8237612588e241c1f1903416bcb43349ca62b97
tengolomidze <88274495+tengolomidze@users.noreply.github.com>
2021-10-09 15:47:15 +0400

    Update app.js

--- app.js
+++ app.js
@@ -108,7 +108,7 @@ canvas.addEventListener("mouseup", function(){
   }
 
   function pushParticles(){
-      for(let i = 0; i < 2; i++){
+      for(let i = 0; i < 5; i++){
           particlesArray.push(new particles())
       }
   }
